d893002ef3449a4e2a31934e7690b3b330536de4
“selam” <Selamkd@users.noreply.github.com>
2024-10-05 19:52:15 +0100

    create react app, add tailwind config, add icon package

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+     <header>
+
+     </header>
+    </div>
+  );
+}
+
+export default App;
