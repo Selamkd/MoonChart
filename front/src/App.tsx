f8aa6bae705533b85aee7cab6f3eda729b157bea
“selam” <Selamkd@users.noreply.github.com>
2024-10-05 20:03:20 +0100

    add components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css';
-
+import { CiLocationOn } from "react-icons/ci";
+import { SlCalender } from "react-icons/sl";
 function App() {
   return (
     <div className="app-container">
-     <header>
-
-     </header>
+        <header className="app-header flex justify-between">
+            {/*Location select*/}
+            <i><CiLocationOn size={28}/></i>
+            <p>Moon Chart</p>
+            <i><SlCalender size={28} /></i>
+        </header>
     </div>
   );
 }
